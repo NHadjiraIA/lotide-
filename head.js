6b1a7c4d957eeb788d161854cb61a8329282f5fc
Hadjira <bkh.hadjira@gmail.com>
2021-06-28 16:55:51 +0000

    Refactoring head

--- head.js
+++ head.js
@@ -1,14 +1,4 @@
-const assertEqual = function(actual, expected) {
- 
-  
-  if (actual === expected) {
-    console.log(` ✅✅✅ Assertion passed: %s == %s, ${ actual }, ${expected}`);
-  } else {
-    console.log(` 🛑🛑🛑 Assertion failed: %s !== %s", ${ actual }, ${expected}`);
-  }
-
-
-};
+const assertEqual = require('./assertEqual');  
 const head = function(data) {
   if (data.length == 0) {
     return undefined;
@@ -16,9 +6,4 @@ const head = function(data) {
     return data[0];
   }
 }
-// TEST CODE
- 
-assertEqual(1, 1);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-console.log(head([5,6,7]))
+
