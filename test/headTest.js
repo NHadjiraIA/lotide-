89cf1649a5e51522ccd307e93608382bca8d0430
Hadjira <bkh.hadjira@gmail.com>
2021-06-28 16:58:05 +0000

    add headTest.js

---
+++ test/headTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require('../assertEqual'); 
+// TEST CODE 
+assertEqual(1, 1);
+assertEqual(head([5,6,7]), 5,"5 is the head of array");
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+ 
